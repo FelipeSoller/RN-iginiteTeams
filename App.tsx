2b990d14ce9807519c3684654c82bdb52144ea79
Felipe Soller <soller@outlook.com>
2024-03-14 22:47:39 -0300

    create Loading component and config status bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 
-import { ActivityIndicator } from 'react-native';
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
 
 import theme from './src/theme';
 
@@ -12,7 +13,8 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      { fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      { fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
