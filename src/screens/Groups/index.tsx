35a46a6dd181191558ec78b789de7e890c110493
Felipe Soller <soller@outlook.com>
2024-03-14 21:41:10 -0300

    refactor Groups screen to use styled components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container } from './styles';
 
 export const Groups = () => {
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
