71aa559a247426c7e76143ea03ec7263967c8526
Felipe Soller <soller@outlook.com>
2024-03-15 16:19:35 -0300

    create Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import { TextInputProps } from "react-native"
+import { Container } from "./styles"
+
+export const Input = ({...rest}: TextInputProps) => {
+  return (
+    <Container {...rest}/>
+  )
+}
